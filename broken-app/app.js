e18a22f08720dfd0d34bb91658983cfaea1cec83
Lisa Jiamsirioungkul <visakhalisajiam@gmail.com>
2021-07-06 12:57:18 -0700

    part 3

--- broken-app/app.js
+++ broken-app/app.js
@@ -2,6 +2,11 @@ const express = require('express');
 let axios = require('axios');
 var app = express();
 
+//take in a JSON body (Github usernames)
+//should return {name, bio}
+//
+
+
 app.post('/', function(req, res, next) {
   try {
     let results = req.body.developers.map(async d => {
@@ -15,4 +20,19 @@ app.post('/', function(req, res, next) {
   }
 });
 
-app.listen(3000);
+
+// 404 handler
+app.use(function (req, res, next) {
+  const notFoundError = new ExpressError("Not Found", 404);
+  return next(notFoundError)
+});
+
+// end generic handler
+app.listen(3000, function (err) {
+  if (err) {
+    console.log(`Error starting server: ${err}`);
+  } else {
+    console.log(`Server starting on port 3000`);
+  }
+});
+
--- broken-app/issues.md
+++ broken-app/issues.md
@@ -1,1 +1,5 @@
 # Broken App Issues
+
+- not using express.json() - express is not able to look for request body
+- generic handler "app.listen(3000)" is not written out
+- not using expresserror.js - 404 error handling
